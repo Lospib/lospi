b40608ff4c8ef02be1f19faed9baad02a02500b5
Lospib <113042491+Lospib@users.noreply.github.com>
2022-09-07 11:58:30 -0300

    Add files via upload

---
+++ lospib.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int num,num2;
+	cout<<"ingresar numero"<<endl;
+	cin>>num;
+	cout<<"ingresar otro numero "<<endl;
+	cin>>num2;
+	
+	return 0;
+}
+
